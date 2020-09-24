e5ab01156c1923c7ada328a19a25f0ff8ee094aa
Oleh_Vls <olehvlasyik@gmail.com>
2020-09-24 14:15:56 +0300

    add swagger for Auth and Operations

--- src/app/operation/operation.controller.ts
+++ src/app/operation/operation.controller.ts
@@ -1,27 +1,42 @@
 import { Body, Controller, Get, Param, Post, UseGuards, BadRequestException } from '@nestjs/common';
-import { OperationService, OperationInterface } from './operation.service';
 import { JwtAuthGuard } from '@src/app/auth/guards';
+import {
+  ApiBearerAuth,
+  ApiOperation,
+  ApiResponse,
+  ApiTags,
+  ApiBody
+} from '@nestjs/swagger';
+import { OperationService } from './operation.service';
 import { operationSchema } from './operation.schemas';
 import { OperationEntity } from './operation.entity';
 
-
+@ApiBearerAuth()
+@ApiTags('operations')
 @Controller('api/operations')
 export class OperationController {
   constructor(private readonly operationService: OperationService) {}
 
   @Get()
+  @ApiOperation({ summary: 'Get all operations' })
+  @ApiResponse({ status: 200, description: 'The found records', type: [OperationEntity] })
   @UseGuards(JwtAuthGuard)
   async getOperations(): Promise<OperationEntity[]> {
     return await this.operationService.getOperations();
   }
 
   @Get('/:id')
+  @ApiOperation({ summary: 'Get operation by id' })
+  @ApiResponse({ status: 200, description: 'The found record', type: OperationEntity })
   @UseGuards(JwtAuthGuard)
   async getOperationById(@Param() params): Promise<OperationEntity> {
     return await this.operationService.getOperationById(params.id);
   }
 
   @Post('/add')
+  @ApiOperation({ summary: 'Add new operation' })
+  @ApiBody({ type: OperationEntity })
+  @ApiResponse({ status: 201, description: 'Operation added', type: OperationEntity })
   @UseGuards(JwtAuthGuard)
   async addOperation(@Body() operationReq): Promise<OperationEntity> {
     try {
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const options = new DocumentBuilder()
+    .setTitle('Finance app')
+    .setDescription('Finance app API description')
+    .setVersion('1.0')
+    .addTag('finance')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3001);
 }
 bootstrap();
